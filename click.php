8fb3497bb02f25b8a5d020a9712775f869be253e
Nile Flores <nile@blondish.net>
2015-05-28 21:15:44 -0500

    Create click.php

---
+++ click.php
@@ -0,0 +1,21 @@
+<?php
+$bannerAdsPath = '/home/yourdomain/public_html/ads/ads.dat';
+require '/home/yourdomain/public_html/ads/ads.inc.php';
+///////////////////////////////////////
+// Don't Edit Anything Below This Line!
+///////////////////////////////////////
+for ($i = 0; $i < count($ads); $i++) {
+    if(ereg('^' .$_GET['id']. '\|\|', $ads[$i])) {
+        $data = explode('||', $ads[$i]);
+        $data[6]++;
+        $ads[$i] = join('||', $data);
+        break;
+    }
+}
+if (!$data[9]) {
+    die();
+}
+writeads();
+Header("Location: $data[9]");
+exit;
+?>
