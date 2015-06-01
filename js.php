67daf772e63c9add81b33bfd0116d77ed2b65f48
Phil Tanner <phil.tanner@sjs.co.nz>
2015-06-01 09:51:08 +0000

    Enable display of default content if no adverts are returned

--- js.php
+++ js.php
@@ -14,7 +14,15 @@ if (!isset($_GET['width']) || !isset($_GET['height']) || !ereg('^[0-9]+$', $_GET
 }
 $buttons = new bannerAds($_GET['id'], 1, $_GET['width'], $_GET['height']);
 header('Content-type: application/x-javascript');
-echo "<!--\n";
-echo "document.write(\"" .addslashes($buttons->ad[0]). "\");\n";
-echo "//-->";
+
+if($buttons && strlen(trim($buttons->ad[0])) )
+{
+    echo "<!--\n";
+    echo "document.write(\"" .addslashes($buttons->ad[0]). "\");\n";
+    echo "//-->";
+} else {
+    echo "<!--\n";
+    echo "document.write(\"".addslashes($bannerAds['default_display'])."\");\n";
+    echo "//-->";
+}
 ?>
